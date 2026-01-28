6c32e33893f96f373e5801c71f689d8773c75765
zhiliang-lin <991788890@qq.com>
2026-01-28 21:14:37 +0100

    add

--- .eleventy.js
+++ .eleventy.js
@@ -587,6 +587,7 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("src/site/img");
+  eleventyConfig.addPassthroughCopy("src/site/html-notes");
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy({ "src/site/logo.*": "/" });
